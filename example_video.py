c218fc41c7f29fb8521f88c2782f759c8471ce2b
levan92 <lingevan0208@gmail.com>
2020-05-21 01:25:35 +0800

    example usage; requirement

---
+++ example_video.py
@@ -0,0 +1,54 @@
+import cv2
+import time
+import argparse
+from pathlib import Path
+
+from yolo import YOLO
+
+parser = argparse.ArgumentParser()
+parser.add_argument('video_path', help='path to video')
+parser.add_argument('--thresh', help='OD confidence threshold', default=0.4, type=float)
+args = parser.parse_args()
+
+assert args.thresh > 0.0
+
+yolo = YOLO(bgr=True, 
+            batch_size=1, 
+            model_path='model_data/yolov3.h5',
+            anchors_path='model_data/yolo_anchors.txt',
+            classes_path='model_data/coco_classes.txt',
+            score=args.thresh,
+            model_image_size=(608, 608)
+            )
+
+vp = Path(args.video_path)
+assert vp.is_file(),'{} not a file'.format(vp)
+cap = cv2.VideoCapture(str(vp))
+assert cap.isOpened(),'Cannot open video file!'
+
+cv2.namedWindow('yolo', cv2.WINDOW_NORMAL)
+
+while True:
+    # Decode
+    ret, frame = cap.read()
+    if not ret:
+        break
+    # Inference
+    dets = yolo.detect_get_box_in([frame], box_format='ltrb')
+    dets = dets[0]
+
+    # Drawing
+    show_frame = frame.copy()
+    for det in dets:
+        ltrb, conf, clsname = det
+        l,t,r,b = ltrb
+        cv2.rectangle(show_frame, (int(l),int(t)),(int(r),int(b)), (255,255,0))
+        cv2.putText(show_frame, '{}:{:0.2f}'.format(clsname, conf), (l,b), cv2.FONT_HERSHEY_DUPLEX, fontScale=1, color=(255,255,0), lineType=2)
+
+    cv2.imshow('yolo', show_frame)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cv2.destroyAllWindows()
+
+ 
